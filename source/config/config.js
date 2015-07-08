abda04d3797e05d331f2e80ab5750ad20fd27ee4
warlyware <danward@gmail.com>
2015-07-08 09:47:07 -0700

    Add tweet box, add tag to tweet, random colors for tags

--- source/config/config.js
+++ source/config/config.js
@@ -5,7 +5,7 @@ angular.module('sif')
   $urlRouterProvider.otherwise('/');
     
   $stateProvider
-  .state('home', {url: '/', templateUrl: '/views/home/home.html'})
+  .state('home', {url: '/', templateUrl: '/views/home/home.html', controller: 'HomeCtrl'})
   .state('user', {url: '', templateUrl: '/views/users/user.html', abstract: true})
   .state('user.register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('user.login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
--- source/index.js
+++ source/index.js
@@ -1,3 +1,32 @@
 'use strict';
 
-angular.module('sif', ['firebase', 'ui.router']);
+var app = angular.module('sif', ['firebase', 'ui.router']);
+
+
+
+app.controller("HomeCtrl", function($scope, $http) {
+  $scope.tags = [], $scope.tweet = '';
+
+  var wordsArr = [];
+  $scope.search = function() {
+    $http.post('http://localhost:8001/search', { words: $scope.words }).success(function(data) {
+      wordsArr = $scope.words.split(' ');
+      console.log(wordsArr.length);
+      $scope.randColor = Please.make_color({
+        colors_returned: wordsArr.length
+      });
+      $scope.tags = data;
+    });
+  }
+
+
+  $scope.sendTweet = function() {
+    $http.post('http://localhost:8001/tweet', {tweet: scope.tweet}).success(function(res) {
+      console.log(res)
+    });
+  }
+
+  $scope.includeInTweet = function(tag) {
+    $scope.tweet = $scope.tweet + ' ' + tag;
+  }
+});
